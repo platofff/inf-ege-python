a3ea405425f034c744985eea68ce53e2a6dc5468
platofff <42435266+platofff@users.noreply.github.com>
2021-06-19 02:27:58 +0200

    Новые решения

--- 19-21/27770.py
+++ 19-21/27770.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
 # Две кучи
-class Combination:
-    def __init__(self, _sum, l):
-        self.sum = _sum
-        self.l = l
+from types import SimpleNamespace
 def iter(k1, k2, n):
     n -= 1
     if n > 0:
-        return Combination(k1 + k2,
-                          [iter(k1 + 1, k2, n),
-                           iter(k1, k2 + 1, n),
-                           iter(k1 * 2, k2, n),
-                           iter(k1, k2 * 3, n)])
-    return Combination(k1 + k2,
-            [(k1 + 1, k2),
-            (k1, k2 + 1),
-            (k1 * 2, k2),
-            (k1, k2 * 3)])
+        return SimpleNamespace(sum=k1 + k2, l=[iter(k1 + 1, k2, n), iter(k1, k2 + 1, n), iter(k1 * 2, k2, n), iter(k1, k2 * 3, n)])
+    return SimpleNamespace(sum=k1 + k2, l=[(k1 + 1, k2), (k1, k2 + 1), (k1 * 2, k2), (k1, k2 * 3)])
 
 for s in range(1, 67):
     combos = iter(16, s, 4)
